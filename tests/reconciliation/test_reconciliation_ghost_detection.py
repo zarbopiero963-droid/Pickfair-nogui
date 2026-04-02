f51f08d5fac8df45f45d5ab8efcd74ea40a5de3e
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-04-02 10:04:38 +0200

    Update test_reconciliation_ghost_detection.py

--- tests/reconciliation/test_reconciliation_ghost_detection.py
+++ tests/reconciliation/test_reconciliation_ghost_detection.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from core.reconciliation_engine import ReconciliationEngine
+from tests.fixtures.fake_batch_manager import FakeBatchManager
 
 
 class FakeDB:
@@ -17,14 +18,22 @@ class FakeBus:
 
 
 def make_engine():
-    return ReconciliationEngine(db=FakeDB(), bus=FakeBus(), batch_manager=None, client_getter=lambda: None)
+    return ReconciliationEngine(
+        db=FakeDB(),
+        bus=FakeBus(),
+        batch_manager=FakeBatchManager(),
+        client_getter=lambda: None,
+    )
 
 
 def test_order_matched_by_bet_id_not_ghost():
     eng = make_engine()
     legs = [{"leg_index": 0, "customer_ref": "R1", "bet_id": "BET1"}]
     remote = [{"customerOrderRef": "XXX", "betId": "BET1", "status": "EXECUTION_COMPLETE"}]
-    ghosts = eng._detect_ghost_orders("B1", legs, remote)
+
+    by_ref, by_bet, by_sel = eng._build_exchange_indices(remote)
+    ghosts = eng._detect_ghost_orders("B1", legs, remote, by_ref, by_bet, by_sel)
+
     assert ghosts == []
 
 
@@ -32,7 +41,10 @@ def test_order_matched_by_customer_ref_not_ghost():
     eng = make_engine()
     legs = [{"leg_index": 0, "customer_ref": "R1", "bet_id": ""}]
     remote = [{"customerOrderRef": "R1", "betId": "BETX", "status": "EXECUTION_COMPLETE"}]
-    ghosts = eng._detect_ghost_orders("B1", legs, remote)
+
+    by_ref, by_bet, by_sel = eng._build_exchange_indices(remote)
+    ghosts = eng._detect_ghost_orders("B1", legs, remote, by_ref, by_bet, by_sel)
+
     assert ghosts == []
 
 
@@ -40,7 +52,10 @@ def test_unmatched_remote_order_is_ghost():
     eng = make_engine()
     legs = [{"leg_index": 0, "customer_ref": "R1", "bet_id": "BET1"}]
     remote = [{"customerOrderRef": "R2", "betId": "BET2", "status": "EXECUTION_COMPLETE"}]
-    ghosts = eng._detect_ghost_orders("B1", legs, remote)
+
+    by_ref, by_bet, by_sel = eng._build_exchange_indices(remote)
+    ghosts = eng._detect_ghost_orders("B1", legs, remote, by_ref, by_bet, by_sel)
+
     assert len(ghosts) == 1
     assert ghosts[0]["customer_ref"] == "R2"
     assert ghosts[0]["bet_id"] == "BET2"
